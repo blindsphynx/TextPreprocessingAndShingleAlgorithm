dafe2edbc860bf86634d1f757eaf9a7ca317b7c3
blindsphynx <violakruglikova98683@gmail.com>
2022-01-23 21:54:42 +0300

    edit processText function

--- src/TextPreprocessingClass.py
+++ src/TextPreprocessingClass.py
@@ -15,15 +15,33 @@ class TextProcessing:
             raise Exception("Empty text or not string")
         self._text = text
 
-    def processText(self):
-        setOfStopWords = set(getDictList(conjunctionPath, prepositionPath,
-                             interjectionPath, particlePath, pronounsPath))
-        punctuationWords = set(getDictList(punctuationPath))
-        for sign in punctuationWords:
+    def collectAllStopWords(self):
+        allStopWords = set(getDictList(conjunctionPath, prepositionPath,
+                                       interjectionPath, particlePath, pronounsPath))
+        punctuationMarks = set(getDictList(punctuationPath))
+        for sign in punctuationMarks:
             self._text = self._text.replace(sign, '')
+        return allStopWords
 
-        resultText = self._text.split(' ')
-        for i in range(0, len(resultText)):
-            if resultText[i].lower().replace('\r', '').replace('\n', '') in setOfStopWords:
-                resultText[i] = ''
-        return ' '.join([value for value in resultText if value != ''])
+    # TODO delete digits function
+
+    def processText(self):
+        setOfStopWords = self.collectAllStopWords()
+
+        self._text = self._text.replace('\r', ' ')
+        tempText = self._text.split(' ')
+        for i in range(0, len(tempText)):
+            if tempText[i].lower().replace('\r', '').replace('\n', '') in setOfStopWords:
+                tempText[i] = ''
+            else:
+                tempText[i] = tempText[i].lower().replace('\n', '')
+
+        resultText = []
+        it = 0
+        while it < len(tempText):
+            if tempText[it] != '':
+                resultText.append(tempText[it])
+                del tempText[it]
+            else:
+                it += 1
+        return resultText
