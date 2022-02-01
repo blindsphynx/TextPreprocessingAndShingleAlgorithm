d0a745d59e9f60e9ad4629b5e99b6cf40299bd71
blindsphynx <violakruglikova98683@gmail.com>
2022-02-01 21:00:22 +0300

    add deleteDigits in TextProcessingClass

--- src/Main.py
+++ src/Main.py
@@ -2,11 +2,11 @@ from TextPreprocessingClass import TextProcessing
 from ShingleAlgorithm import divideTextIntoShingles
 import codecs
 
-path = "C:\\Users\\Lenovo\\Desktop\\Text.txt"
+path = "C:\\Users\\Lenovo\\Desktop\\digits.txt"
 file = codecs.open(path, encoding='utf-8')
 
-# print(Text.processText())
-Text = TextProcessing(file.read()).processText()
-file.close
-# print(len(Text))
-print(divideTextIntoShingles(Text))
+Text = TextProcessing(file.read())
+print(Text.processText())
+# Text = TextProcessing(file.read()).processText()
+file.close()
+# print(divideTextIntoShingles(Text))
